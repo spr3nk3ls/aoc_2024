eaecdb4746477116b4972168594285813e169baf
Rik Sprenkels <spr3nk3ls@gmail.com>
2024-12-04 10:35:58 +0100

    Day 4: small refactoring

--- src/main/kotlin/spr3nk3ls/day4/Day4.kt
+++ src/main/kotlin/spr3nk3ls/day4/Day4.kt
@@ -14,10 +14,11 @@ private fun getSolutionA(filename: String) {
     val rows = Utils.readLines(filename)
     val rowCount = rows.sumOf { countXmas(it) }
     val colCount = getCols(rows).sumOf { countXmas(it) }
-    val diag1Count = getDiag1(rows).sumOf { countXmas(it) }
-    val diag2Count = getDiag2(rows).sumOf { countXmas(it) }
-    val diag3Count = getDiag3(rows).sumOf { countXmas(it) }
-    val diag4Count = getDiag4(rows).sumOf { countXmas(it) }
+    val max = rows.size - 1
+    val diag1Count = getDiag1(rows, max).sumOf { countXmas(it) }
+    val diag2Count = getDiag2(rows, max).sumOf { countXmas(it) }
+    val diag3Count = getDiag3(rows, max).sumOf { countXmas(it) }
+    val diag4Count = getDiag4(rows, max).sumOf { countXmas(it) }
     println(rowCount + colCount + diag1Count + diag2Count + diag3Count + diag4Count)
 }
 
@@ -29,24 +30,20 @@ private fun getCols(lines: List<String>): List<String> {
     return lines.indices.map { i -> lines.map { line -> line[i] }.joinToString("") }
 }
 
-private fun getDiag1(lines: List<String>): List<String> {
-    val max = lines.size - 1
+private fun getDiag1(lines: List<String>, max: Int): List<String> {
     return (0..max).map { n -> (0..n).map { i -> lines[i][n - i] }.joinToString("") }
 }
 
-private fun getDiag2(lines: List<String>): List<String> {
-    val max = lines.size - 1
-    return (0..max - 1).map { n -> (0..n).map { i -> lines[max - i][max - n + i] }.joinToString("").reversed() }
+private fun getDiag2(lines: List<String>, max: Int): List<String> {
+    return (0..<max).map { n -> (0..n).map { i -> lines[max - i][max - n + i] }.joinToString("").reversed() }
 }
 
-private fun getDiag3(lines: List<String>): List<String> {
-    val max = lines.size - 1
+private fun getDiag3(lines: List<String>, max: Int): List<String> {
     return (0..max).map { n -> (0..n).map { i -> lines[max - i][n - i] }.joinToString("") }
 }
 
-private fun getDiag4(lines: List<String>): List<String> {
-    val max = lines.size - 1
-    return (0..max - 1).map { n -> (0..n).map { i -> lines[i][max - n + i] }.joinToString("").reversed() }
+private fun getDiag4(lines: List<String>, max: Int): List<String> {
+    return (0..<max).map { n -> (0..n).map { i -> lines[i][max - n + i] }.joinToString("").reversed() }
 }
 
 private fun getSolutionB(filename: String) {
