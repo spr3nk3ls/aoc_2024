6649463f87edfaab516c8baab06c5f3849f42b0d
Rik Sprenkels <spr3nk3ls@gmail.com>
2024-12-06 10:49:45 +0100

    Day 6, more improvements.

--- src/main/kotlin/spr3nk3ls/day6/Day6.kt
+++ src/main/kotlin/spr3nk3ls/day6/Day6.kt
@@ -12,21 +12,21 @@ private fun getSolution(filename: String) {
     val range = lines.first().length to lines.size
     val obstacles = scanLines(lines, range, '#').toSet()
     val index = scanLines(lines, range, '^').first()
-    val pointer = Pointer(index, 0 to -1)
+    val guard = Guard(index, 0 to -1)
 
     // A
-    val sequence = generateSequence(pointer) { move(it, obstacles) }.takeWhile { withinBounds(it.index, range) }
-    val result = sequence.map { it.index }.toSet()
-    println(result.count())
+    val route = generateSequence(guard) { it.move(obstacles) }.takeWhile { withinBounds(it.index, range) }
+    val positions = route.map { it.index }.toSet()
+    println(positions.count())
 
     // B
-    val obstaclesWithAdditional = result.map { obstacles + it }
-    val resultB = obstaclesWithAdditional.count { withAdditional ->
-        val sequenceWithAdditional = generateSequence(pointer) { move(it, withAdditional) }
+    val obstaclesWithExtra = positions.map { obstacles + it }
+    val numberOfLoops = obstaclesWithExtra.count { withExtra ->
+        val routeWithExtraObstacle = generateSequence(guard) { it.move(withExtra) }
             .takeWhile { withinBounds(it.index, range) }
-        detectLoop(sequenceWithAdditional)
+        detectLoop(routeWithExtraObstacle)
     }
-    println(resultB)
+    println(numberOfLoops)
 }
 
 private fun scanLines(lines: List<String>, range: Pair<Int, Int>, character: Char): List<Pair<Int, Int>> {
@@ -37,27 +37,21 @@ private fun scanLines(lines: List<String>, range: Pair<Int, Int>, character: Cha
     }
 }
 
-private fun move(pointer: Pointer, obstacles: Set<Pair<Int, Int>>): Pointer {
-    val index = pointer.index
-    val direction = pointer.direction
-    val newIndex = (index.first + direction.first) to (index.second + direction.second)
-    return if (newIndex in obstacles)
-        Pointer(index, rotate(direction))
-    else
-        Pointer(newIndex, direction)
-}
-
-private fun rotate(direction: Pair<Int, Int>): Pair<Int, Int> {
-    return (-direction.second to direction.first)
+private data class Guard(val index: Pair<Int, Int>, val direction: Pair<Int, Int>){
+    fun move(obstacles: Set<Pair<Int, Int>>): Guard {
+        val newIndex = (index.first + direction.first) to (index.second + direction.second)
+        return if (newIndex in obstacles)
+            Guard(index, -direction.second to direction.first)
+        else
+            Guard(newIndex, direction)
+    }
 }
 
 private fun withinBounds(index: Pair<Int, Int>, range: Pair<Int, Int>): Boolean {
     return index.first >= 0 && index.first < range.first && index.second >= 0 && index.second < range.second
 }
 
-private fun detectLoop(sequence: Sequence<Pointer>): Boolean {
-    val seen = mutableSetOf<Pointer>()
+private fun detectLoop(sequence: Sequence<Guard>): Boolean {
+    val seen = mutableSetOf<Guard>()
     return sequence.any { !seen.add(it) }
 }
-
-private data class Pointer(val index: Pair<Int, Int>, val direction: Pair<Int, Int>)
