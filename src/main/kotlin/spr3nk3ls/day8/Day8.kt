ca854ce5eeee1d2ba80dd1e38e4c081629dde2dc
Rik Sprenkels <spr3nk3ls@gmail.com>
2024-12-08 15:21:45 +0100

    Day 8, cleanup.

--- src/main/kotlin/spr3nk3ls/day8/Day8.kt
+++ src/main/kotlin/spr3nk3ls/day8/Day8.kt
@@ -13,43 +13,41 @@ private fun getSolution(filename: String, project: (Node, Node, Pair<Int, Int>) 
     val lines = Utils.readLines(filename)
     val range = lines.first().length to lines.size
     val nodes = scanLines(lines, range).toSet()
-    val antinodes = nodes.groupBy { it.frequency }.flatMap { freqNodes ->
-        freqNodes.value.flatMap { freqNode1 ->
-            freqNodes.value.filter { it != freqNode1 }.flatMap { freqNode2 ->
-                project.invoke(freqNode1, freqNode2, range)
+    val antinodes = nodes.groupBy { it.frequency }.flatMap { (_, freqNodes) ->
+        freqNodes.flatMap { freqNode1 ->
+            freqNodes.filter { it != freqNode1 }.flatMap { freqNode2 ->
+                project(freqNode1, freqNode2, range)
             }
         }
     }.toSet()
     println(antinodes.size)
 }
 
-private fun scanLines(lines: List<String>, range: Pair<Int, Int>): List<Node> {
-    return (0..<range.second).flatMap { y ->
-        (0..<range.first)
-            .filter { lines[y][it] != '.' }
-            .map {
-                Node((it to y), lines[y][it])
-            }
+private fun scanLines(lines: List<String>, range: Pair<Int, Int>): List<Node> = (0..<range.second).flatMap { y ->
+    (0..<range.first).filter { lines[y][it] != '.' }.map {
+        Node((it to y), lines[y][it])
     }
 }
 
 private data class Node(val index: Pair<Int, Int>, val frequency: Char) {
+    private fun calculateDifference(otherNode: Node): Pair<Int, Int> =
+        (index.first - otherNode.index.first) to (index.second - otherNode.index.second)
+
     fun projectOne(otherNode: Node, range: Pair<Int, Int>): Set<Pair<Int, Int>> {
-        val difference = (index.first - otherNode.index.first) to (index.second - otherNode.index.second)
-        return listOf(index.first + difference.first to index.second + difference.second)
-            .filter { withinBounds(it, range) }
-            .toSet()
+        val difference = calculateDifference(otherNode)
+        val projected = index.first + difference.first to index.second + difference.second
+        return if (withinBounds(projected, range)) setOf(projected) else emptySet()
     }
 
     fun projectAll(otherNode: Node, range: Pair<Int, Int>): Set<Pair<Int, Int>> {
-        val difference = (index.first - otherNode.index.first) to (index.second - otherNode.index.second)
-        return generateSequence(0) { it + 1 }
-            .map { index.first + it * difference.first to index.second + it * difference.second }
-            .takeWhile { withinBounds(it, range) }
-            .toSet()
+        val difference = calculateDifference(otherNode)
+        return generateSequence(index) { (x, y) -> x + difference.first to y + difference.second }.takeWhile {
+            withinBounds(
+                it, range
+            )
+        }.toSet()
     }
 }
 
-private fun withinBounds(index: Pair<Int, Int>, range: Pair<Int, Int>): Boolean {
-    return index.first >= 0 && index.first < range.first && index.second >= 0 && index.second < range.second
-}
+private fun withinBounds(index: Pair<Int, Int>, range: Pair<Int, Int>): Boolean =
+    index.first >= 0 && index.first < range.first && index.second >= 0 && index.second < range.second
