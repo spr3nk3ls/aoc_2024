9e17efba95fcf1f337a1bade85310ba33215c6a1
Rik Sprenkels <spr3nk3ls@gmail.com>
2024-12-09 11:53:56 +0100

    Day 9, part 2

--- src/main/kotlin/spr3nk3ls/day9/Day9.kt
+++ src/main/kotlin/spr3nk3ls/day9/Day9.kt
@@ -2,19 +2,24 @@ package spr3nk3ls.day9
 
 import spr3nk3ls.util.Utils
 
-private val emptyFile = File(0, -1)
-
 fun main() {
-    getSolution("day9/example.txt")
-    getSolution("day9/input.txt")
+    getSolutionA("day9/example.txt")
+//    getSolutionA("day9/input.txt")
+    getSolutionB("day9/example.txt")
+    getSolutionB("day9/input.txt")
 }
 
-private fun getSolution(filename: String) {
+private fun getSolutionA(filename: String) {
     val line = Utils.readLines(filename).first()
-    var unwrappedLines = unwrap(line)
-    while (unwrappedLines.contains(emptyFile)) {
+    val files = line.indices.map { index ->
+        if (index % 2 == 0) File(index / 2, line[index].digitToInt()) else File(
+            -1, line[index].digitToInt()
+        )
+    }
+    var unwrappedLines = unwrap(files)
+    while (unwrappedLines.any { it.idNumber == -1 }) {
         val last = unwrappedLines.removeLast()
-        if (last != emptyFile) {
+        if (last.idNumber != -1) {
             insertInto(unwrappedLines, last)
         }
     }
@@ -23,20 +28,52 @@ private fun getSolution(filename: String) {
     })
 }
 
-private fun unwrap(line: String): MutableList<File> {
-    return line.indices.flatMap { index ->
-        val diskSpace = line[index].digitToInt()
-        if (index % 2 == 0) {
-            (0..<diskSpace).map { File(index / 2, diskSpace) }
-        } else {
-            (0..<diskSpace).map { File(0, -1) }
-        }
+private fun unwrap(files: List<File>): MutableList<File> {
+    return files.indices.flatMap { index ->
+        val diskSpace = files[index].diskSpace
+        (0..<diskSpace).map { files[index] }
     }.toMutableList()
 }
 
 private fun insertInto(unwrappedLines: MutableList<File>, last: File) {
-    val index = unwrappedLines.indexOfFirst { it == emptyFile }
+    val index = unwrappedLines.indexOfFirst { it.idNumber == -1 }
     unwrappedLines[index] = last
+}
+
+private fun getSolutionB(filename: String) {
+    val line = Utils.readLines(filename).first()
+    val immutableFiles = line.indices.map { index ->
+        if (index % 2 == 0) File(index / 2, line[index].digitToInt()) else File(
+            -1, line[index].digitToInt()
+        )
+    }
+    var files = immutableFiles.toMutableList()
+    val reversed = files.reversed().filter { it.idNumber != -1 }
+
+    reversed.forEach {
+        putInto(files, it)
+    }
+
+    val unwrapped = unwrap(files)
+    println(unwrapped.indices.filter { unwrapped[it].idNumber != -1 }.sumOf {
+        (it * unwrapped[it].idNumber).toLong()
+    })
+}
+
+private fun putInto(files: MutableList<File>, fileToInsert: File): MutableList<File> {
+    files.indices.forEach { index ->
+        val file = files[index]
+        val toInsert = file.idNumber == -1 && (file.diskSpace >= fileToInsert.diskSpace) && index < files.lastIndexOf(fileToInsert)
+        if(toInsert) {
+            val emptySpace = files[index]
+            val removedIndex = files.indexOfLast { it == fileToInsert }
+            files[removedIndex] = File(-1, fileToInsert.diskSpace)
+            files[index] = fileToInsert
+            files.add(index + 1, File(-1, emptySpace.diskSpace - fileToInsert.diskSpace))
+            return files
+        }
+    }
+    return files
 }
 
 private data class File(val idNumber: Int, val diskSpace: Int)
