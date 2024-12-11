a1dfa18e33096aded09cf03710bc3fa75c11d9a3
Rik Sprenkels <spr3nk3ls@gmail.com>
2024-12-11 09:42:34 +0100

    Day 11 part 1 (now with recursion and caching).

--- src/main/kotlin/spr3nk3ls/day11/Day11.kt
+++ src/main/kotlin/spr3nk3ls/day11/Day11.kt
@@ -4,26 +4,50 @@ import spr3nk3ls.util.Utils
 
 
 fun main() {
-    getSolution("day11/example.txt")
-    getSolution("day11/input.txt")
+    getSolution("day11/example.txt", 25)
+    getSolution("day11/input.txt", 25)
+    getSolution("day11/example.txt", 75)
+    getSolution("day11/input.txt", 75)
 }
 
-fun getSolution(filename: String) {
-    var numbers = Utils.readLines(filename).first().split(" ").map { it.toLong() }
-    for(i in 0 until 25) {
-        numbers = numbers.flatMap {
-            if (it == 0L) {
-                listOf(1)
-            } else {
-                val str = it.toString()
-                val ndig = str.length
-                if (ndig % 2 == 0) {
-                    listOf(str.substring(0, ndig / 2).toLong(), str.substring(ndig / 2).toLong())
-                } else {
-                    listOf(it * 2024)
-                }
-            }
+fun getSolution(filename: String, blinks: Int) {
+    val stones = Utils.readLines(filename).first().split(" ").map { it.toLong() }
+    var cache = mutableMapOf<Pair<Long, Int>, Long>()
+    val numberOfStones = stones.sumOf {
+        transformStones(it, blinks, cache)
+    }
+    println(numberOfStones)
+}
+
+private fun transformStones(stone: Long, index: Int, cache: MutableMap<Pair<Long, Int>, Long>): Long {
+    val cacheKey = stone to index
+    if (cache.containsKey(cacheKey)) {
+        return cache[cacheKey]!!
+    }
+    if (index == 0) {
+        return 1
+    }
+    if (stone == 0L) {
+        val res = transformStones(1, index - 1, cache)
+        cache[cacheKey] = res
+        return res
+    } else {
+        val str = stone.toString()
+        val ndig = str.length
+        if (ndig % 2 == 0) {
+            val res = transformStones(
+                str.substring(0, ndig / 2).toLong(), index - 1, cache
+            ) + transformStones(
+                str.substring(ndig / 2).toLong(), index - 1, cache
+            )
+            cache[cacheKey] = res
+            return res
+        } else {
+            val res = transformStones(stone * 2024, index - 1, cache)
+            cache[cacheKey] = res
+            return res
         }
     }
-    println(numbers.size)
 }
+
+
