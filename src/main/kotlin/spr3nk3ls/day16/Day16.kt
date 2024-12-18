d8638937a7ce9f6dea967d0c8daba4d5a9beed47
Rik Sprenkels <spr3nk3ls@gmail.com>
2024-12-18 16:13:09 +0100

    Day 16 part 1

--- src/main/kotlin/spr3nk3ls/day16/Day16.kt
+++ src/main/kotlin/spr3nk3ls/day16/Day16.kt
@@ -1,6 +1,7 @@
 package spr3nk3ls.day16
 
 import spr3nk3ls.util.Utils
+import java.util.*
 import kotlin.math.abs
 
 private enum class Direction(val dir: Pair<Int, Int>) {
@@ -9,15 +10,33 @@ private enum class Direction(val dir: Pair<Int, Int>) {
 
 fun main() {
     // 7036
-    getSolution("day16/example.txt")
-//    getSolutionA("day16/input.txt")
+    getSolution("day16/example1.txt")
+    getSolution("day16/example2.txt")
+    getSolution("day16/input.txt")
 }
 
 fun getSolution(filename: String) {
     val grid = Utils.readLines(filename)
-    val nodeMap = getNodeMap(grid)
-    //TODO implement Dijkstra
-    nodeMap.entries.forEach(::println)
+    val nodeMap = getNodeMap(grid).toMutableMap()
+    val start = grid.indices.flatMap { y ->
+        grid[y].indices.filter { x ->
+            grid[y][x] == 'S'
+        }.map { it to y }
+    }.first()
+    val end = grid.indices.flatMap { y ->
+        grid[y].indices.filter { x ->
+            grid[y][x] == 'E'
+        }.map { it to y }
+    }.first()
+    nodeMap[start] = nodeMap[start]!!.map { node ->
+        if (node.first.first == 1) {
+            node.first to node.second + 1000
+        } else {
+            node
+        }
+    }
+    val result = dijkstra(nodeMap, start)
+    println(result[end]!! - 1000)
 }
 
 private fun getNodeMap(grid: List<String>): Map<Pair<Int, Int>, List<Pair<Pair<Int, Int>, Int>>> {
@@ -45,10 +64,32 @@ private fun getNodeMap(grid: List<String>): Map<Pair<Int, Int>, List<Pair<Pair<I
 }
 
 private fun corner(connections: Set<Direction>): Boolean {
-    return (connections.contains(Direction.NORTH) && connections.contains(Direction.EAST)) ||
-            (connections.contains(Direction.NORTH) && connections.contains(Direction.WEST)) ||
-            (connections.contains(Direction.SOUTH) && connections.contains(Direction.EAST)) ||
-            (connections.contains(Direction.SOUTH) && connections.contains(Direction.WEST))
+    return (connections.contains(Direction.NORTH) && connections.contains(Direction.EAST)) || (connections.contains(
+        Direction.NORTH
+    ) && connections.contains(Direction.WEST)) || (connections.contains(Direction.SOUTH) && connections.contains(
+        Direction.EAST
+    )) || (connections.contains(Direction.SOUTH) && connections.contains(Direction.WEST))
+}
+
+private fun dijkstra(
+    graph: Map<Pair<Int, Int>, List<Pair<Pair<Int, Int>, Int>>>, start: Pair<Int, Int>
+): Map<Pair<Int, Int>, Int> {
+    val distances = mutableMapOf<Pair<Int, Int>, Int>().withDefault { Int.MAX_VALUE }
+    val priorityQueue = PriorityQueue<Pair<Pair<Int, Int>, Int>>(compareBy { it.second }).apply { add(start to 0) }
+
+    distances[start] = 0
+
+    while (priorityQueue.isNotEmpty()) {
+        val (node, currentDist) = priorityQueue.poll()
+        graph[node]?.forEach { (adjacent, weight) ->
+            val totalDist = currentDist + weight + 1000
+            if (totalDist < distances.getValue(adjacent)) {
+                distances[adjacent] = totalDist
+                priorityQueue.add(adjacent to totalDist)
+            }
+        }
+    }
+    return distances
 }
 
 fun isHallway(pair: Pair<Int, Int>, grid: List<String>): Boolean {
