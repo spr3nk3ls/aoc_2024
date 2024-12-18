05518e7aff5ccedfa20685ff75762331735a1167
Rik Sprenkels <spr3nk3ls@gmail.com>
2024-12-18 08:53:19 +0100

    Day 16 construct correct graph.

--- src/main/kotlin/spr3nk3ls/day16/Day16.kt
+++ src/main/kotlin/spr3nk3ls/day16/Day16.kt
@@ -1,86 +1,56 @@
 package spr3nk3ls.day16
 
 import spr3nk3ls.util.Utils
+import kotlin.math.abs
 
 private enum class Direction(val dir: Pair<Int, Int>) {
     NORTH(0 to -1), EAST(1 to 0), SOUTH(0 to 1), WEST(-1 to 0)
 }
 
 fun main() {
-        // 7036
+    // 7036
     getSolution("day16/example.txt")
 //    getSolutionA("day16/input.txt")
 }
 
 fun getSolution(filename: String) {
     val grid = Utils.readLines(filename)
+    val nodeMap = getNodeMap(grid)
+    //TODO implement Dijkstra
+    nodeMap.entries.forEach(::println)
+}
+
+private fun getNodeMap(grid: List<String>): Map<Pair<Int, Int>, List<Pair<Pair<Int, Int>, Int>>> {
     val nodes = grid.indices.flatMap { y ->
-        grid[y].indices.map { x ->
-            val startOrEnd = grid[y][x] == 'S' || grid[y][x] == 'E'
-            if (isHallway(x to y, grid)) {
-                val connections = Direction.entries
-                    .filter { grid[it.dir.second + y][it.dir.first + x] == '.' }.toMutableSet()
-                if (connections.size >= 3 || startOrEnd) {
-                    (y to x) to connections
-                } else {
-                    null
-                }
-            } else {
-                null
-            }
-        }
-    }.filterNotNull()
-    nodes.forEach { node -> println(node) }
+        grid[y].indices.filter { x ->
+            isHallway(x to y, grid)
+        }.map { x ->
+            x to Direction.entries.filter { grid[it.dir.second + y][it.dir.first + x] == '.' }.toMutableSet()
+        }.filter { (x, connections) ->
+            corner(connections) || grid[y][x] == 'S' || grid[y][x] == 'E'
+        }.map { (x, connections) -> (x to y) to connections }
+    }
     val nodeSet = nodes.unzip().first.toSet()
-    val result = nodes.map { node ->
+    return nodes.associate { node ->
         val nodeConnections = node.second.map {
             it.dir
-        }.filter { isHallway(node.first.first + it.first to node.first.second + it.second, grid)
-        }.map { initDir ->
-            var pos = node.first.first + initDir.first to node.first.second + initDir.second
-            var dir = initDir
-            while (pos !in nodeSet){
-                var nextPos = pos
-                while(isHallway(nextPos, grid)){
-                    pos = nextPos
-                    nextPos = pos.first + dir.first to pos.second + dir.second
-                }
-                var newDir = (-dir.second to dir.first)
-                if(isHallway(pos.first + newDir.first to pos.second + newDir.second, grid)){
-                    dir = newDir
-                    continue
-                }
-                newDir = (dir.second to -dir.first)
-                if(isHallway(pos.first + newDir.first to pos.second + newDir.second, grid)){
-                    dir = newDir
-                    continue
-                }
-                println("dead end")
-                println(pos)
-                // Dead end
-                break
-            }
-            (dir to pos)
-//            println(pos)
-//            println()
-//            var entry = nodeMap[node.first]
-//            if(entry == null) {
-//                nodeMap[node.first] = mutableMapOf()
-//                entry = nodeMap[node.first]
-//            }
-//            entry!!.put(dir, pos)
-            //calculate distance to second node or null if dead end
-            //connect both nodes (add other node to map if unavailable)
-            //then remove this direction and opposite direction of other node
-        }.toMap()
+        }.flatMap { dir ->
+            generateSequence(node.first.first + dir.first to node.first.second + dir.second) { it.first + dir.first to it.second + dir.second }.takeWhile {
+                isHallway(it, grid)
+            }.filter { it in nodeSet }
+                .map { it to abs(node.first.first - it.first) + abs(node.first.second - it.second) }
+        }
         node.first to nodeConnections
-//        println(nodeConnections)
-//    }.filter { it.second.isNotEmpty()
-    }.toMap()
-    result.entries.forEach(::println)
-//    println(nodes)
+    }
+}
+
+private fun corner(connections: Set<Direction>): Boolean {
+    return (connections.contains(Direction.NORTH) && connections.contains(Direction.EAST)) ||
+            (connections.contains(Direction.NORTH) && connections.contains(Direction.WEST)) ||
+            (connections.contains(Direction.SOUTH) && connections.contains(Direction.EAST)) ||
+            (connections.contains(Direction.SOUTH) && connections.contains(Direction.WEST))
 }
 
 fun isHallway(pair: Pair<Int, Int>, grid: List<String>): Boolean {
-    return grid[pair.second][pair.first] in setOf('.','S','E')
+    return grid[pair.second][pair.first] in setOf('.', 'S', 'E')
 }
