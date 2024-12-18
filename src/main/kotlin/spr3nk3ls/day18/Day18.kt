3b566a8a1a296009c25c900cd680e48a02721f7b
Rik Sprenkels <spr3nk3ls@gmail.com>
2024-12-18 23:08:12 +0100

    Day 18 part 2

--- src/main/kotlin/spr3nk3ls/day18/Day18.kt
+++ src/main/kotlin/spr3nk3ls/day18/Day18.kt
@@ -3,22 +3,18 @@ package spr3nk3ls.day18
 import spr3nk3ls.util.Utils
 
 fun main() {
-    getSolution("day18/example.txt", 6, 12)
-    // 140 too low
-    getSolution("day18/input.txt", 70, 1024)
+    getSolutionA("day18/example.txt", 6, 12)
+    getSolutionA("day18/input.txt", 70, 1024)
+    getSolutionB("day18/example.txt", 6, 12)
+    getSolutionB("day18/input.txt", 70, 1024)
 }
 
-fun getSolution(filename: String, range: Int, corruption: Int) {
-    val corrupted = Utils.readLines(filename).take(corruption).map {
-        it.split(",").map { it.toInt() }
-    }.map { it.first() to it.last() }
-    val grid = (0..range).flatMap { y ->
-        (0..range).filter { x -> x to y !in corrupted }.map { x -> x to y }
-    }.toMutableSet()
+fun getSolutionA(filename: String, range: Int, corruption: Int) {
+    val (grid, _) = getGrid(filename, corruption, range)
     val deque = ArrayDeque<List<Pair<Int, Int>>>()
     deque.add(listOf(0 to 0))
     grid.remove(0 to 0)
-    while (deque.first().last() != range to range) {
+    while (deque.isEmpty() || deque.first().last() != range to range) {
         val route = deque.removeFirst()
         available(route.last(), grid).forEach { av ->
             deque.add(route + av)
@@ -26,6 +22,38 @@ fun getSolution(filename: String, range: Int, corruption: Int) {
         }
     }
     println(deque.first().size - 1)
+}
+
+fun getSolutionB(filename: String, range: Int, initialCorruption: Int) {
+    outer@ for (corruption in generateSequence(initialCorruption) { it + 1 }) {
+        val (grid, lastCorrupted) = getGrid(filename, corruption, range)
+        val deque = ArrayDeque<List<Pair<Int, Int>>>()
+        deque.add(listOf(0 to 0))
+        grid.remove(0 to 0)
+        while (deque.first().last() != range to range) {
+            val route = deque.removeFirst()
+            available(route.last(), grid).forEach { av ->
+                deque.add(route + av)
+                grid.remove(av)
+            }
+            if (deque.isEmpty()) {
+                println(lastCorrupted)
+                break@outer
+            }
+        }
+    }
+}
+
+private fun getGrid(
+    filename: String, corruption: Int, range: Int
+): Pair<MutableSet<Pair<Int, Int>>, Pair<Int, Int>> {
+    val corrupted = Utils.readLines(filename).take(corruption).map {
+        it.split(",").map { it.toInt() }
+    }.map { it.first() to it.last() }
+    val grid = (0..range).flatMap { y ->
+        (0..range).filter { x -> x to y !in corrupted }.map { x -> x to y }
+    }.toMutableSet()
+    return grid to corrupted.last()
 }
 
 private fun available(node: Pair<Int, Int>, grid: MutableSet<Pair<Int, Int>>): List<Pair<Int, Int>> {
