1756bdcb00d397d70eb4ff1cb3268814d5d710a0
Rik Sprenkels <spr3nk3ls@gmail.com>
2024-12-19 08:43:44 +0100

    Day 19 part 2, naive brute force search that does not work.

--- src/main/kotlin/spr3nk3ls/day19/Day19.kt
+++ src/main/kotlin/spr3nk3ls/day19/Day19.kt
@@ -3,11 +3,13 @@ package spr3nk3ls.day19
 import java.io.File
 
 fun main() {
-    getSolution("day19/example.txt")
-    getSolution("day19/input.txt")
+    getSolutionA("day19/example.txt")
+    getSolutionA("day19/input.txt")
+    getSolutionB("day19/example.txt")
+    getSolutionB("day19/input.txt")
 }
 
-fun getSolution(filename: String) {
+fun getSolutionA(filename: String) {
     val (towels, patterns) = readAndSplitFile(filename)
     val result = patterns.count { pattern ->
         var matches = setOf("")
@@ -22,9 +24,36 @@ fun getSolution(filename: String) {
     println(result)
 }
 
-private fun readAndSplitFile(filename: String): Pair<Set<String>, List<String>> {
+fun getSolutionB(filename: String) {
+    val (towels, patterns) = readAndSplitFile(filename)
+    val total = patterns.size
+    val result = patterns.sumOf { pattern ->
+        val index = patterns.indexOf(pattern)
+        println("Taken $index of $total patterns")
+        var matches = setOf(listOf(""))
+        var count = 0
+        val len = pattern.length
+        while (matches.isNotEmpty()) {
+            matches = matches
+                .asSequence()
+                .flatMap { match -> towels.map { match + it } }
+                .map { it to it.joinToString("") }
+                .filter { (_, str) -> pattern.startsWith(str) }
+                .filter { (_, str) -> str.length <= len }
+                .map { (ls, _) -> ls }
+                .toSet()
+            val exactMatches = matches.filter { it.joinToString("") == pattern }
+            count += exactMatches.size
+            matches = matches.filter { it !in exactMatches }.toSet()
+        }
+        count
+    }
+    println(result)
+}
+
+private fun readAndSplitFile(filename: String): Pair<List<String>, List<String>> {
     val (tow, pat) = File("src/main/resources/$filename").readText(Charsets.UTF_8).split("\n\n")
     val towels = tow.split(", ")
     val patterns = pat.split("\n")
-    return towels.toSet() to patterns
+    return towels to patterns
 }
